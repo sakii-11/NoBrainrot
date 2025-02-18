570bb0d18bca55ebcb7502f417537e46d31ef5d1
Your New Username <149531974+sakii-11@users.noreply.github.com>
2025-02-19 00:43:15 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+    <h1>No Brainrot</h1>
+    <h3 >Learn, Don’t Rot</h3>
+    </div>
+  )
+}
+
+export default App
